badaffc081dd7e2ad68af6471f27ef10de2d7827
tvtvtv9810 <tvtvtv9810@gmail.com>
2020-11-03 17:56:30 +0900

    ページが外観の作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,73 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <h1>Hello, world</h1>
+<React.Fragment>
+<header className="PageHeader_header">
+    <h1 className="PageHeader_title">Todoアプリ</h1>
+    <nav>
+      <ul className="PageHeader_nav">
+        <li>テストユーザさん</li>
+        <li>ログアウト</li>
+      </ul>
+    </nav>
+  </header>
+  <div className="TodoBoard_content">
+    <div className="TodoForm_content">
+      <form className="TodoForm_form">
+        <div className="TodoForm_input">
+          <input type="text" placeholder="タスクを入力してください"/>
+        </div>
+        <div className="TodoForm_button">
+          <button type="button">追加</button>
+        </div>
+      </form>
+    </div>
+    <div className="TodoFilter_content">
+      <button className="TodoFilter_buttonSelected">全て</button>
+      <button className="TodoFilter_buttonUnselected">未完了のみ</button>
+      <button className="TodoFilter_buttonUnselected">完了のみ</button>
+    </div>
+
+    <ul className="TodoList_list">
+      <li className="TodoItem_item">
+        <div className="TodoItem_todo">
+          <label>
+            <input type="checkbox" className="TodoItem_checkbox" checked={true} />
+            <span>洗い物をする</span>
+          </label>
+        </div>
+        <div className="TodoItem_delete">
+          <button className="TodoItem_button">x</button>
+        </div>
+      </li>
+      <li className="TodoItem_item">
+        <div className="TodoItem_todo">
+          <label>
+            <input type="checkbox" className="TodoItem_checkbox" />
+            <span>洗濯物を干す</span>
+          </label>
+        </div>
+        <div className="TodoItem_delete">
+          <button className="TodoItem_button">x</button>
+        </div>
+      </li>
+      <li className="TodoItem_item">
+        <div className="TodoItem_todo">
+          <label>
+            <input type="checkbox" className="TodoItem_checkbox" />
+            <span>買い物へ行く</span>
+          </label>
+        </div>
+        <div className="TodoItem_delete">
+          <button className="TodoItem_button">x</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  </React.Fragment>
   );
 }
 
